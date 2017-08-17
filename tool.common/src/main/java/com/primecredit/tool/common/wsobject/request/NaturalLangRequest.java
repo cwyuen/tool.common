dbf142e13efde6704c83e9a9a7479e0a8c0a08d9
maxwellyuen <maxwellyuen@192.168.19.116>
2017-08-17 09:51:56 +0800

    Signed-off-by: maxwellyuen <maxwellyuen@192.168.19.116>

--- tool.common/src/main/java/com/primecredit/tool/common/wsobject/request/NaturalLangRequest.java
+++ tool.common/src/main/java/com/primecredit/tool/common/wsobject/request/NaturalLangRequest.java
@@ -1,11 +1,15 @@
 package com.primecredit.tool.common.wsobject.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.primecredit.tool.common.domain.NaturaLangEntry;
 import com.primecredit.tool.common.wsobject.BaseWsObject;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class NaturalLangRequest extends BaseWsObject{
 	private String input;
+	private String sourceFile; 
+	private int line; 
+	private NaturaLangEntry entry;
 
 	public String getInput() {
 		return input;
@@ -13,5 +17,29 @@ public class NaturalLangRequest extends BaseWsObject{
 
 	public void setInput(String input) {
 		this.input = input;
+	}
+
+	public String getSourceFile() {
+		return sourceFile;
+	}
+
+	public void setSourceFile(String sourceFile) {
+		this.sourceFile = sourceFile;
+	}
+
+	public int getLine() {
+		return line;
+	}
+
+	public void setLine(int line) {
+		this.line = line;
+	}
+
+	public NaturaLangEntry getEntry() {
+		return entry;
+	}
+
+	public void setEntry(NaturaLangEntry entry) {
+		this.entry = entry;
 	}
 }
